8995daca84d083c3297fc745cdf7c09c22f4621f
Grzegorz Zima <grzegorz.zima@gmail.com>
2022-03-10 13:34:50 +0100

    Removed commented-out code

--- server-app/src/Program.cs
+++ server-app/src/Program.cs
@@ -2,9 +2,6 @@ using SimpleGrpcService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
